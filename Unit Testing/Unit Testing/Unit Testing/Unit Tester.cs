1ec8dc643ae6b1559d4d5b6adb074b6b08b32c63
65001 <asathiabalan@gmail.com>
2017-09-04 22:20:16 -0400

    Unit Testing Project
    Adds NUnit and NUnit3TestingAdapter

---
+++ Unit Testing/Unit Testing/Unit Testing/Unit Tester.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using DBM;
+using LitDev;
+using Microsoft.SmallBasic;
+using NUnit.Framework;
+
+namespace Unit_Testing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        } 
+    }
+
+    [TestFixture]
+    class Import
+    {
+        [Test]
+        public void CSV()
+        {
+            Assert.That( () => DBM.Import.CSV(""), Throws.TypeOf<FileNotFoundException>());
+        }
+    }
+
+    [TestFixture]
+    class Export
+    {
+
+    }
+}
